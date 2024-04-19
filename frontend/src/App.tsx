fddb3054275b7dcf60512409a481753853724afe
joao <joao@pop-os.localdomain>
2024-04-19 18:43:51 +0100

    setup frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-pink-400 text-white text-3xl'>Daniel GAY</h1>
+    </>
+  )
+}
+
+export default App
