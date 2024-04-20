51c5cc4c859f65f0c5bfe2e8db5787789ac93b25
Miguel Matos <miguelamatos@ua.pt>
2024-04-20 14:59:09 +0100

    changes in requirements

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,34 +1,34 @@
 from vision import OpenAIIntegration
-from schema import ImageInput
+# from schema import ImageInput
 from typing import Optional
-from config import CONFIG
-import torch
-from model import Model
-import shutil   
+# from config import CONFIG
+# # import torch
+# from model import Model
+# import shutil   
 
 from fastapi import FastAPI, Form, File, UploadFile
-from fastapi.logger import logger
+# from fastapi.logger import logger
 import uvicorn
-from predict import predict
+# from predict import predict
 
 app = FastAPI()
 
-@app.on_event("startup")
-async def startup_event():
-    """
-    Initialize FastAPI and add variables
-    """
+# @app.on_event("startup")
+# async def startup_event():
+#     """
+#     Initialize FastAPI and add variables
+#     """
 
-    # logger.info('Running envirnoment: {}'.format(CONFIG['ENV']))
-    # logger.info('PyTorch using device: {}'.format(CONFIG['DEVICE']))
+#     # logger.info('Running envirnoment: {}'.format(CONFIG['ENV']))
+#     # logger.info('PyTorch using device: {}'.format(CONFIG['DEVICE']))
 
-    # Initialize the pytorch model
-    model = Model()
+#     # Initialize the pytorch model
+#     # model = Model()
     
-    # add model and other preprocess tools too app state
-    app.package = {
-        "model": model
-    }
+#     # add model and other preprocess tools too app state
+#     app.package = {
+#         "model": model
+#     }
 
 @app.get("/")   
 async def root():
