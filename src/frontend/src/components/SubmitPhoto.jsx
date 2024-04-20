471b583d2e874728d66ffcd0210bdc9f1a87b4f9
Daniel <danielmartinsferreira@ua.pt>
2024-04-21 00:48:39 +0100

    missing bins only

--- src/frontend/src/components/SubmitPhoto.jsx
+++ src/frontend/src/components/SubmitPhoto.jsx
@@ -2,44 +2,46 @@ import React, { useContext } from 'react';
 import { AiFillSecurityScan } from "react-icons/ai";
 import { LuScan } from "react-icons/lu";
 import { VisionContext } from '../contexts/VisionContext';
+import { MdRecycling } from "react-icons/md";
 import '../App.css';
 
 function SubmitPhoto() {
 
   const { handleButtonClick, handleFileChange, fileInputRef, loading } = useContext(VisionContext);
+
   return (
-    <div className="box-border flex flex-col justify-around h-full">
-      <div className="flex flex-col gap-2 mt-20 h-fit">
-        <h1 className="text-5xl font-bold">Bem-vind@ ao <span className="font-black tracking-wider text-primary ">FIMPR</span></h1>
-      </div>  
-      <div className="flex flex-col gap-4">
-        <form>
-          <input
-            type="file"
-            accept="image/*" // Only accept images
-            ref={fileInputRef}
-            style={{ display: 'none' }}
-            onChange={handleFileChange}
-          />
-          <button
-            type="button"
-            className="px-4 py-4 btn btn-primary h-fit text-primary-content animate-pulse"
-            onClick={handleButtonClick}>
-            <div className="h-fit text-8xl">
-              <LuScan />
-            </div>
-            <div>
-              <span className="text-2xl font-bold">FIMPaR</span>
-              <p className="font-normal">usa a nossa ferramenta de reconhecimento de resíduos</p>
-            </div>
-          </button>
-        </form>
-      </div>
-      {loading && (
-        <div className="flex items-center justify-center">
-          <div className="w-12 h-12 mb-4 ease-linear border-8 border-t-8 border-gray-200 rounded-full loader"></div>
-          <span className="text-xl font-semibold">Processing image...</span>
-        </div>
+    <div className={`box-border flex flex-col ${loading ? 'justify-start' : 'justify-around'} h-full`}>
+      {!loading ? (
+        <>
+          <div className="flex flex-col gap-2 mt-20 h-fit">
+            <h1 className="text-5xl font-bold">Bem-vind@ ao <span className="font-black tracking-wider text-primary ">FIMPR</span></h1>
+          </div>
+          <div className="flex flex-col gap-4">
+            <form>
+              <input
+                type="file"
+                accept="image/*" // Only accept images
+                ref={fileInputRef}
+                style={{ display: 'none' }}
+                onChange={handleFileChange}
+              />
+              <button
+                type="button"
+                className="px-4 py-4 btn btn-primary h-fit text-primary-content animate-pulse"
+                onClick={handleButtonClick}>
+                <div className="h-fit text-8xl">
+                  <LuScan />
+                </div>
+                <div>
+                  <span className="text-2xl font-bold">FIMPaR</span>
+                  <p className="font-normal">usa a nossa ferramenta de reconhecimento de resíduos</p>
+                </div>
+              </button>
+            </form>
+          </div>
+        </>
+      ) : (
+            <div className="h-48 mt-5 w-96 skeleton"></div>
       )}
     </div>
   );
--- src/frontend/src/pages/Analysis.jsx
+++ src/frontend/src/pages/Analysis.jsx
@@ -1,31 +1,53 @@
-import React, { useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import Top from "../components/Top";
 import { VisionContext } from "../contexts/VisionContext";
-import { MdRecycling } from "react-icons/md";
-import { IoArrowBack } from "react-icons/io5";
-import { FaArrowAltCircleDown } from "react-icons/fa";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 function Analysis() {
     const { info } = useContext(VisionContext);
+    const [recyclable, setRecyclable] = useState(true);
+
+    useEffect(() => {
+        if (info) {
+            setRecyclable(info.recyclable.toLowerCase() === 'no' ? false : true);
+        }
+    }, [info]);
     useEffect(() => {
         AOS.init({
-            // settings here can be adjusted based on your needs, for example:
-            duration: 1200, // controls animation duration
+            duration: 2000,
+            once: true
         });
     }, []);
+
+    useEffect(() => {
+        AOS.refresh();
+    }, [recyclable]);
+    console.log('recyclable:', recyclable)
     return (
         <div className="flex flex-col items-center justify-center h-fit gap-4 overflow-hidden h-[calc(100vh*2 - 32px)]">
-            <div className="flex flex-col items-center h-screen gap-4 text-center" data-aos="fade-up">
-                <div className="flex flex-col items-center gap-2">
-                        <div>
-                            <p>Recyclable: {info.recyclable}</p>
-                            <p>Instructions: {info.instructions}</p>
-                            <p>Bins: {info.bins}</p>
-                        </div>
-                    ))}
+            <Top></Top>
+            <div className={`card w-96 text-primary-content ${recyclable ? 'bg-primary' : 'bg-secondary'}`} data-aos="fade-up" data-aos-delay="200">
+                <div className="card-body">
+                    <h2 className="text-5xl card-title">is it recyclable?</h2>
+                    <p className="text-2xl">{info.recyclable}!</p>
                 </div>
             </div>
+
+            <div className={`card w-96 text-primary-content bg-info ${recyclable ? '' : 'hidden'}`} data-aos="fade-up" data-aos-delay="2000">
+                <div className="card-body">
+                    <h2 className="text-5xl card-title">how do I recycle it?</h2>
+                    <p className="text-2xl">{info.instructions}</p>
+                </div>
+            </div>
+
+            <div className={`card w-96 text-accent bg-neutral ${recyclable ? '' : 'hidden'}`} data-aos="fade-up" data-aos-delay="3000">
+                <div className="card-body">
+                    <h2 className="text-5xl card-title">where would I recycle it?</h2>
+                    <p className="text-2xl">{info.bins}</p>
+                </div>
+            </div>
+
         </div>
     );
 }
