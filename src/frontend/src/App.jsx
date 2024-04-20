f819c40b7533d965d76396cc4fcf1b2d60ac903e
Daniel <danielmartinsferreira@ua.pt>
2024-04-20 18:54:37 +0100

    decluterring frontend

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,19 +1,14 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Auth from './pages/Auth'
-import Register from './pages/Register'
 import AppShell from './pages/AppShell'
 import Analysis from './pages/Analysis'
 
 function App() {
 
   return (
-    <div className='p-4 max-w-screen overflow-x-hidden bg-base-100 max-h-screen h-screen'>
+    <div className='h-screen max-h-screen p-4 overflow-x-hidden max-w-screen bg-base-100'>
       <Router>
         <Routes>
-        {/* AUTH */}
-          <Route path="/login" element={<Auth></Auth>} />
-          <Route path="/register" element={<Register></Register>} />
         {/* Landing */}
           <Route path="/" element={<AppShell></AppShell>} />
           <Route path="/teste" element={<Analysis></Analysis>} />
--- src/frontend/src/pages/AppShell.jsx
+++ src/frontend/src/pages/AppShell.jsx
@@ -1,10 +1,5 @@
 import Top from "../components/Top"
-import { FaBookOpenReader } from "react-icons/fa6";
-import { AiFillSecurityScan } from "react-icons/ai";
-import { LuScan } from "react-icons/lu";
-import Landing from "./Landing";
-import Perfil from "./Perfil";
-import Scan from "./Scan";
+import Landing from "../components/Landing";
 
 
 
@@ -12,13 +7,11 @@ function AppShell() {
 
 
   return (
-    <div className="flex flex-col max-h-screen box-border justify-around">
-        <Top></Top>
-        <div className="h-[calc(100vh-96px)] flex flex-col">
+    <div className="box-border flex flex-col justify-around max-h-screen">
+      <Top></Top>
+      <div className="h-[calc(100vh-96px)] flex flex-col">
         <Landing></Landing>
-        {/*<Perfil></Perfil>*/}
-        {/*<Scan></Scan>*/}
-        </div>
+      </div>
     </div>
   );
 }
