b3a6ede9b9331e626ba07061bcf3f190dce18454
Daniel <danielmartinsferreira@ua.pt>
2024-04-21 11:21:11 +0100

    fixed margin ratios

--- src/frontend/src/pages/Analysis.jsx
+++ src/frontend/src/pages/Analysis.jsx
@@ -47,25 +47,31 @@ function Analysis() {
         }
     }, [info, getMapsLink]); 
 
-
+    useEffect(() => {
+        // Scroll to the bottom of the page after the component mounts
+        window.scrollTo({
+            top: document.documentElement.scrollHeight,
+            behavior: 'smooth'
+        });
+    }, []);
     return (
         <div className="flex flex-col items-center justify-around h-fit gap-4 overflow-hidden h-[calc(100vh*2 - 32px)]">
             <Top></Top>
-            <div className={`card w-96 text-primary-content ${recyclable ? 'bg-primary' : 'bg-secondary'}`} data-aos="fade-up" data-aos-delay="200">
+            <div className={`card w-[90dvw] text-primary-content ${recyclable ? 'bg-primary' : 'bg-secondary'}`} data-aos="fade-up" data-aos-delay="200">
                 <div className="card-body">
                     <h2 className="text-5xl card-title">recyclable?</h2>
                     <p className="text-2xl">{info.recyclable}!</p>
                 </div>
             </div>
 
-            <div className={`card w-96 text-primary-content bg-info ${recyclable ? '' : 'hidden'}`} data-aos="fade-up" data-aos-delay="2000">
+            <div className={`card w-[90dvw] text-primary-content bg-info ${recyclable ? '' : 'hidden'}`} data-aos="fade-up" data-aos-delay="2000">
                 <div className="card-body">
                     <h2 className="text-5xl card-title">how?</h2>
                     <p className="text-2xl">{info.instructions}</p>
                 </div>
             </div>
 
-            <div className={`card w-96 text-neutral-content bg-neutral ${recyclable ? '' : 'hidden'}`} data-aos="fade-up" data-aos-delay="2800">
+            <div className={`card w-[90dvw] text-neutral-content bg-neutral ${recyclable ? '' : 'hidden'}`} data-aos="fade-up" data-aos-delay="2800">
                 <div className="card-body">
                     <div className="flex justify-between">
                         <div className="text-5xl card-title">where?</div>
